4c94b0621b266dc94d3236f02c5c885c061d4d12
+ <+>
2023-10-31 14:32:56 +0300

    Completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deletQuiz }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,51 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    console.log("Delete button clicked");
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error("Error deleting the question:", response.status, response.statusText);
+          throw new Error(`Error deleting the question: ${response.status} ${response.statusText}`);
+        }
+        console.log("Question deleted successfully");
+        return response.json();
+      })
+      .then(() => {
+        console.log("Question deleted from the server");
+        deletQuiz(id);
+      })
+      .catch((error) => {
+        console.error("Error deleting the question:", error);
+      });
+  }
+
+  function changeAnswer(event) {
+    const correctIndex = parseInt(event.target.value);
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    });
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={changeAnswer}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,34 @@
 import React from "react";
+import {useState ,useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [query,setQueries] = useState([])
+  const[ques,setQues] = useState([])
+
+useEffect(()=>{
+  fetch(`http://localhost:4000/questions`)
+  .then((res) => res.json())
+  .then((data) => {setQueries(data)}
+)},[])
+
+function deletQuiz (id){
+ const update = ques.filter((ques)=>ques.id !== id);
+  setQues(update)
+} 
+
+const Questions =( query.map((item)=>{
+  return <div key={item.id}>
+     <QuestionItem 
+   question={item} deletQuiz={deletQuiz} 
+  />
+  </div>
+}))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{Questions}</ul>
     </section>
   );
 }
